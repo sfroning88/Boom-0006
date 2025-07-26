1dea6ceb91a9f9daf51dd8a779ccb0b603f236b2
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-26 09:10:41 -0500

    Update brennan.py

--- agents/brennan.py
+++ agents/brennan.py
@@ -1,7 +1,6 @@
 agent_id = https://vapi.ai?demo=true&shareKey=a0550dae-732b-4e64-89e9-bd1fbaf1d15b&assistantId=373d7e61-e201-4d83-90c9-b3709d6e28f4
 
-agent_description =
-'''
+agent_description = '''
 # Sales Leads Agent Prompt
 
 ## Identity & Purpose
