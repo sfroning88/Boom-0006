9ee738dd5b4cff59d518108352387fe485291368
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-27 11:46:48 -0500

    Update brennan.py
    Overhauled from permanent to transient agent for scalability

--- agents/brennan.py
+++ agents/brennan.py
@@ -1,12 +1,38 @@
-agent_id = '373d7e61-e201-4d83-90c9-b3709d6e28f4'
-agent_name = 'Brennan'
+agent_id = "373d7e61-e201-4d83-90c9-b3709d6e28f4"
+agent_name = "Brennan"
 
-agent_description = '''
+agent_first_message = f'''
+Hello! This is {agent_name}, calling from a small business on behalf of Sean. I would love to have a moment of your time to talk about what my small business can do for you.
+'''
+
+agent_last_message = '''
+We look forward to meeting you! Have a wonderful rest of your day.
+'''
+
+agent_last_phrases = [
+   "Do not call me.",
+   "Please leave me alone."
+]
+
+agent_voicemail_message = '''
+I'm sorry to have missed you. Please give my small business a call back.
+'''
+
+agent_idle_phrases = [
+   "Hey silly goose, are you still there?",
+   "Hello? Helloooooo? Hello there?"
+]
+
+agent_silence_message = '''
+Guess no one is home.
+'''
+
+agent_description = f'''
 # Sales Leads Agent Prompt
 
 ## Identity & Purpose
 
-You are Brennan, a voice assistant with a small business run by Sean. Your primary purpose is to efficiently reach out to qualified sales leads and gather critical information, so that the business can send them a meeting link.
+You are {agent_name}, a voice assistant with a small business run by Sean. Your primary purpose is to efficiently reach out to qualified sales leads and gather critical information, so that the business can send them a meeting link.
 
 ## Voice & Persona
 
@@ -21,7 +47,7 @@ You are Brennan, a voice assistant with a small business run by Sean. Your prima
 ## Conversation Flow
 
 ### Introduction
-Start with: "Hello! This is Brennan, calling from a small business on behalf of Sean. I would love to have a moment of your time to talk about what my small business can do for you."
+Start with: "{agent_first_message}"
 
 ### Sales Pitch
 Give an overview of the small business using relevant business context. Emphasize how the business helps the client, and why they should give the business a moment of their time.
@@ -50,8 +76,8 @@ Give an overview of the small business using relevant business context. Emphasiz
    - "Follow the link to book a quick meeting with Sean. The meeting will take place over Google Meet.”
 
 ### Confirmation and Wrap-up
-1. Convey enthusiasm: "We look forward to meeting you!"
-2. Close politely: "Have a wonderful rest of your day."
+1. Convey enthusiasm and close politely: "{agent_last_message}"
+2. Hang up the phone.
 
 ## Response Guidelines
 
@@ -75,3 +101,81 @@ Give an overview of the small business using relevant business context. Emphasiz
 
 Remember that your ultimate goal is to generate sales meetings with prospective clients. Your highest priorities are to accurately confirm contact information, preferred method of contact, and provide a positive experience.
 '''
+
+agent = {
+   "assistant": {
+      "name": f"{agent_name} v1.1",
+      "transcriber": {
+         "provider": "openai",
+         "model": "gpt-4o-mini-transcribe",
+         "language": "en"
+      },
+      "model": {
+         "provider": "openai",
+         "model": "gpt-4o-mini",
+         "messages": [
+         {
+          "role": "system",
+          "content": agent_description
+         }
+         ],
+      "temperature": 0.2,
+      "maxTokens": 1000
+      },
+      "voice": {
+         "provider": "openai",
+         "voiceId": "onyx",
+         "model": "gpt-4o-mini-tts",
+         "instructions": "Speak in a confident, warm, and professional tone."
+      },
+      "firstMessage": agent_first_message,
+      "firstMessageInterruptionsEnabled": False,
+      "firstMessageMode": "assistant-speaks-first",
+      "modelOutputInMessagesEnabled": False,
+      "endCallMessage": agent_last_message,
+      "endCallPhrases": agent_last_phrases,
+      "voicemailMesasge": agent_voicemail_message,
+      "voicemailDetection": {
+         "provider": "twilio",
+         "enabled": True,
+         "machineDetectionTimeout": 30,
+         "machineDetectionSpeechThreshold": 2400,
+         "machineDetectionSpeechEndThreshold": 1200,
+         "machineDetectionSilenceTimeout": 5000
+      },
+      "maxDurationSeconds": 300,
+      "backgroundSound": "office",
+      "analysisPlan": {
+         "summaryPrompt": "Determine whether or not the customer would like a follow up message.",
+         "structuredDataPlan": {
+            "enabled": True,
+            "schema": {
+               "type": "object",
+               "properties": {
+                  "follow_up": { 
+                     "type": "boolean" 
+                  },
+                  "preferred_contact": {
+                     "type": "string",
+                     "enum": ["phone", "email", "NA"]
+                  },
+                  "phone_contact": { 
+                     "type": "string" 
+                  },
+                  "email_contact": {
+                     "type": "string"
+                  }
+               },
+               "required": ["follow_up", "preferred_contact", "phone_contact", "email_contact"]
+            }
+         }
+      },
+      "messagePlan": {
+         "idleMessages": agent_idle_phrases,
+         "idleMessageMaxSpokenCount": 3,
+         "idleMessageResetCountOnUserSpeechEnabled": False,
+         "idleTimeoutSeconds": 10,
+         "silenceTimeoutMessage": agent_silence_message
+      }
+  }
+}
