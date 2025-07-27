7e74abb9fae3335ed1ed3c4f83e6674324533168
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-27 18:16:46 -0500

    Update message.py
    Added in text functionality

--- api/message.py
+++ api/message.py
@@ -1,8 +1,26 @@
-def send_message(response, method):
-    pass
+def send_text(booking_link, twilio_token, twilio_account, business_number, customer_number):
+    from twilio.rest import Client
 
-def send_text(response):
-    pass
+    agent_message = f'''
+    Hello! Thank you for taking the time earlier to speak about what my small business can do for you.
+    Go ahead and book a 15 minute meeting with Sean using this link: 
+    {booking_link}
+
+    Best,
+    Small Business
+    '''
+
+    # Create Twilio client
+    client = Client(twilio_account, twilio_token)
+
+    # Send SMS using client
+    message = client.messages.create(
+        body=agent_message,
+        from_=business_number,
+        to=customer_number
+    )
+    
+    return True
 
 def send_email(response):
     pass
