29eacd88601a63780a8cce464e398e17442ecd7f
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-27 18:39:56 -0500

    Update message.py
    Full SMS functionality, email template

--- api/message.py
+++ api/message.py
@@ -1,4 +1,4 @@
-def send_text(booking_link, twilio_token, twilio_account, business_number, customer_number):
+def send_text(booking_link, twilio_token, twilio_account, sender, receiver):
     from twilio.rest import Client
 
     agent_message = f'''
@@ -14,13 +14,24 @@ def send_text(booking_link, twilio_token, twilio_account, business_number, custo
     client = Client(twilio_account, twilio_token)
 
     # Send SMS using client
+    # NOTE: AWAITING TOLL FREE VERIFICATION
     message = client.messages.create(
         body=agent_message,
-        from_=business_number,
-        to=customer_number
+        from_=sender,
+        to=receiver
     )
     
     return True
 
-def send_email(response):
-    pass
+def send_email(booking_link, twilio_token, twilio_account, sender, receiver):
+    
+    agent_message = f'''
+    Hello! Thank you for taking the time earlier to speak about what my small business can do for you.
+    Go ahead and book a 15 minute meeting with Sean using this link: 
+    {booking_link}
+
+    Best,
+    Small Business
+    '''
+
+    return True
