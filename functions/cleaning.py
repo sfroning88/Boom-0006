ee2093994fdab70aa4159a8b73c30ee4b86b17fe
Sean Froning <123010146+sfroning88@users.noreply.github.com>
2025-07-27 18:40:25 -0500

    Create cleaning.py
    Placeholders for cleaning

---
+++ functions/cleaning.py
@@ -0,0 +1,11 @@
+def clean_phone(dirty_phone):
+    
+    
+    clean_result = dirty_phone
+    return clean_result
+
+def clean_email(dirty_email):
+
+
+    clean_result = dirty_email
+    return clean_result
